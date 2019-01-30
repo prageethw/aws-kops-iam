5d84009ca228e8c6534952df3ef911fc21b5869a
Prageeth Warnak <prageeth.warnak@team.telstra.com>
2019-01-30 20:19:56 +1100

    chaos monkey addons added
    Signed-off-by: Prageeth Warnak <prageeth.warnak@team.telstra.com>

---
+++ cluster/addons/chaos-monkey/deploy.sh
@@ -0,0 +1,3 @@
+echo "wait till chaos monkey deployed as defined in Chaosfile.json "
+cat Chaosfile.json  | sed -e  "s@NODE_ASG@$ASG_NAME@g" | tee chaos_file.temp.json
+chaos-lambda deploy -c chaos-file.json
